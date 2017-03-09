55354d0ebbc124f803a2619bff424950156a80d4
hatelove <jay.hatelove@gmail.com>
2017-03-09 15:46:03 +0800

    initialized and add the first failed test case: num is 5, single should be 5

---
+++ LeetCode_136_Single_Number/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LeetCode_136_Single_Number
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void nums_is_5_singleNumber_should_be_5()
+        {
+            var nums = new int[] {5};
+
+            Assert.AreEqual(5, new Solution().SingleNumber(nums));
+        }
+    }
+
+    public class Solution
+    {
+        public int SingleNumber(int[] nums)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
